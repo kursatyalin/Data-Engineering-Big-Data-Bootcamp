ebf4c790f20b710b9453e147d95fe2aa1ad77a26
Kürşat YALIN <kursat@africanmanagers.org>
2023-09-15 11:09:21 +0000

    dim

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -6,11 +6,23 @@ payments as (
     select * from {{ref('stg_payments')}}
 ),
 
+order_payments as (
+    select 
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+    from payments
+    group by 1
+),
+
 final as (
-    select orders.order_id,
+    select 
+        orders.order_id,
         orders.customer_id,
-        payments.amount
-    from orders left join payments using (order_id)
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+        
+    from orders 
+    left join order_payments using (order_id)
 )
 
 select * from final
